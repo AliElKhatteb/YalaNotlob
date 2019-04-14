9eb0e270f530b670271ecf9af80bb9b379a12171
Dina Gamal <dinag707@gmail.com>
2019-04-14 16:20:35 +0200

    complete active_storage model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
  
 
 def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
 
-
+    
 end
