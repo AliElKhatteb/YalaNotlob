b4407021f90880a0b9e207db7ac147cab465206e
“Nada2222git config --global user.name “Nada2222 <nadamohamad498@yahoo.com>
2019-04-14 20:58:41 +0200

    complrting add order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,22 +5,37 @@ class OrdersController < ApplicationController
 
     def create_orders
         @order = Order.new
-        @order.order_for = 
-        @order.rest_name = 
-        @order.
+        @order.rest_name = params[:rest_name]
+        @order.order_for = params[:order_for]
+        @order.status = "waiting"
+        @order.user =  current_user
+
+        @order.save
+        redirect_to action: "view_all"
+        puts "hereeee"
+        puts @order
+    end 
     def view_all
+        puts "viiiiiiiiewwwww"
+
         @orders = Order.where(:user_id => current_user.id)
     end
     def finish
+        puts "finnnnnnishhhhhh"
         order=Order.find(params[:id])
         order.update_attribute(:status, "finished")
         redirect_to action: "view_all" 
 
     end
-    def cancel()
+    def cancel
     
         Order.find(params[:id]).destroy
         redirect_to action: "view_all" 
+
+    end
+    def addFriendGroup
+        params[:friends]
+        redirect_to action: "view_orders"
 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   get '/' ,to: 'home#index'
   get '/orders' , to: 'orders#view_all'
+  get '/orders/add' , to: 'orders#view_orders'
+
   delete '/orders/:id' , to: 'orders#cancel', as: "order_path_delete"
   post '/orders/:id' , to: 'orders#finish', as: "order_path_finish"
   post '/orders/:id/items',to: 'items#create_item'
   get '/orders/:id/items' ,to: 'items#view_items' , as: "order_items"
 
-  get '/orders' , to: 'orders#view_orders'
-  post '/orders' , to: 'order#create_orders'
+  post '/orders/add' , to: 'orders#create_orders'
+  
+  post '/orders/addfriend' ,to: 'ordres#addFriendGroup' ,as: "addFriendGroup"
   # order_addfriend_url
  # devise_for :users
 #devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
